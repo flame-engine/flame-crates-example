a1f9df377d6a608df7f89bfdf557f6a25f057556
Luan Nico <luanpotter27@gmail.com>
2017-10-26 23:16:57 -0200

    Fetching screen dimensions

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,23 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
 
-void main() {
+main() async {
   Flame.util.enableEvents();
   Flame.audio.disableLog();
 
-  new MyGame().start();
+  var dimensions = await Flame.util.initialDimensions();
+  new MyGame(dimensions).start();
 }
 
 class MyGame extends Game {
 
+  Size dimensions;
+
+  MyGame(this.dimensions);
+
   @override
   void render(Canvas canvas) {
-    var rect = new Rect.fromLTWH(10.0, 10.0, 100.0, 100.0);
+    var rect = new Rect.fromLTWH(100.0, 100.0, dimensions.width - 200.0, dimensions.height - 200.0);
     var paint = new Paint()..color = new Color(0xFFFF0000);
     canvas.drawRect(rect, paint);
   }
