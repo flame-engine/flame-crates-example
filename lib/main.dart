364a6c20cc096050503c9dbca9797c68a9b13738
Luan Nico <luanpotter27@gmail.com>
2017-10-29 08:50:40 -0200

    kill the boxes

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,59 @@ import 'package:flame/game.dart';
 import 'package:flame/component.dart';
 import 'package:flame/flame.dart';
 
+const SPEED = 100.0;
+const CRATE_SIZE = 128.0;
+
 main() async {
   Flame.util.enableEvents();
   Flame.audio.disableLog();
 
   var dimensions = await Flame.util.initialDimensions();
-  new MyGame(dimensions).start();
+  var game = new MyGame(dimensions)..start();
+  window.onPointerDataPacket = (packet) {
+    var pointer = packet.data.first;
+    game.input(pointer.physicalX, pointer.physicalY);
+  };
 }
 
 class Crate extends SpriteComponent {
-  Crate() : super.square(128.0, 'crate.png') {
+  Crate() : super.square(CRATE_SIZE, 'crate.png') {
     this.angle = 0.0;
   }
 }
 
-const SPEED = 100;
-
 class MyGame extends Game {
 
   Size dimensions;
-  Crate crate;
+  List<Crate> crates = [];
 
   MyGame(this.dimensions) {
-    this.crate = new Crate();
-    this.crate.x = dimensions.width / 2;
-    this.crate.y = 200.0;
+    crates.add(crate(dimensions.width / 2));
+  }
+
+  static Crate crate(double x) {
+    Crate crate = new Crate();
+    crate.x = x;
+    crate.y = 200.0;
+    return crate;
   }
 
   @override
   void render(Canvas canvas) {
-    crate.render(canvas);
+    crates.forEach((crate) => crate.render(canvas));
   }
 
   @override
   void update(double t) {
-    crate.y += t * SPEED;
+    crates.forEach((crate) => crate.y += t * SPEED);
+  }
+
+  void input(double x, double y) {
+    crates.removeWhere((crate) {
+      double dx = (crate.x - x).abs();
+      double dy = (crate.y - y).abs();
+      var diff = CRATE_SIZE / 2;
+      return (dx < diff && dy < diff);
+    });
   }
 }
